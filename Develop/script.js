7018174ca3bf517ba8df05232b68510621303250
Vanity <ktkyletran@gmail.com>
2021-01-09 16:45:38 -0600

    Added functions to generate random letter, number, and symbol

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var hasLower = confirm("Would you like lower case letters?");
+var hasUpper = confirm("Would you like upper case letters?");
+var hasNumber = confirm("Would you like numbers?");
+var hasSymbol = confirm("Would you like symbols?");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +14,25 @@ function writePassword() {
 
 }
 
+// Random Generator Function Choices -- Traversy Media on YouTube
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSymbol() {
+  var symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-console.log(randomstring = Math.random().toString(36).slice(-8));
+
