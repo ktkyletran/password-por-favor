dd190ad51d99ff7072bc77b5cbdc4647f96cc8f2
Vanity <ktkyletran@gmail.com>
2021-01-11 15:38:45 -0600

    Changed generatePassword() function, added answer validation, and linked functions to button

--- Develop/script.js
+++ Develop/script.js
@@ -9,46 +9,62 @@ function writePassword() {
   var hasNumber = confirm("Would you like numbers?");
   var hasSymbol = confirm("Would you like symbols?");
   var passwordLength = parseInt(prompt("How many characters would you like in your password? Pick between 8 and 128"));
+  
   // Variables to be included/excluded in generated password
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
   var symbols = "!@#$%^&*()[]<>.,";
+
   // Puts generated password in password box
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  // Function to generate random password
+
+  // Function to generate random password -- w/ help from VerkkoNet via YouTube
   function generatePassword() {
     var allowedOptions = "";
-    var abbey = "";
+    var generatedPassword = "";
     var length = passwordLength;
 
+    // Check to include selected options
     if (hasLower === true) {
       allowedOptions += lowercase;
     }
+
     if (hasUpper === true) {
       allowedOptions += uppercase;
     }
+    
     if (hasNumber === true) {
       allowedOptions += numbers;
     }
+
     if (hasSymbol === true) {
       allowedOptions += symbols;
     }
-    if (passwordLength >= 8 && passwordLength <= 128) {
+
+    // Check for password length and at least one option selected before returning password
+    if (passwordLength >= 8 && passwordLength <= 128 && allowedOptions !== "") {
       for (var i = 0; i < length; i++) {
         var random = Math.floor(Math.random() * allowedOptions.length);
   
-        abbey += allowedOptions[random];
+        generatedPassword += allowedOptions[random];
       }
-      return abbey;
-    } else {}
-
+      return generatedPassword;
+    } 
+    else if (allowedOptions !== "" && passwordLength > 128 || passwordLength < 8) {
+      return "Please try again and choose a number between 8 and 128";
+    }
+    else if (allowedOptions === "" && passwordLength > 128 || passwordLength < 8) {
+      return "Please choose at least one password option and choose a number between 8 and 128";
+    }
+    else {
+      return "Please start over and select at least one option \n(Lowercase, Uppercase, Numbers, and/or Symbols)";
+    }
   };
 
   passwordText.value = password;
 };
 
-console.log(password);
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
